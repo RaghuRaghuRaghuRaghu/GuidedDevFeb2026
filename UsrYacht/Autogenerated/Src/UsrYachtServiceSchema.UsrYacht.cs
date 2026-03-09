b2e3d3bc5d1c48733b3057b483c1f44461171574
RaghuRaghuRaghuRaghu <kbraghavendra@zertain.com>
2026-03-09 16:47:36 +0530

    Day 8 - Wow

---
+++ UsrYacht/Autogenerated/Src/UsrYachtServiceSchema.UsrYacht.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrYachtServiceSchema
+
+	/// <exclude/>
+	public class UsrYachtServiceSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrYachtServiceSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrYachtServiceSchema(UsrYachtServiceSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("4b4b0daf-3ea7-4eae-a540-637e86daf8f0");
+			Name = "UsrYachtService";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("c4d50eaf-6a76-4da5-9fce-1482155810c9");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,83,40,206,207,77,85,72,206,207,207,81,72,86,6,210,41,169,10,25,169,69,169,138,138,138,0,53,68,155,91,26,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("4b4b0daf-3ea7-4eae-a540-637e86daf8f0"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
